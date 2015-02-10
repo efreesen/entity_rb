014b71d625b05f090e6dd19768bc4b4350c14ecc
caltorres <Caio Alexandre Torres>
2015-02-10 18:34:23 -0200

    Fixed bugs for repeated fields and for shared fields between child classes

--- lib/entity_rb/base.rb
+++ lib/entity_rb/base.rb
@@ -1,16 +1,22 @@
 module Entity
   class Base
-    @@fields = []
-
     def initialize(attributes={})
       attributes.each do |key, value|
-        send("#{key}=", value) if @@fields.include? key.to_sym
+        send("#{key}=", value) if self.class.fields.include? key.to_sym
       end
     end
 
     protected
+    def self.fields
+      @fields
+    end
+
     def self.field(key)
       raise ArgumentError.new('Key must be a String, Symbol or Array') unless [String, Symbol, Array].include? key.class
+
+      @fields ||= []
+
+      p @fields
 
       if key.is_a? Array
         key.each do |k|
@@ -20,7 +26,7 @@ module Entity
           end
         end
       else
-        @@fields.push key.to_sym
+        @fields.push key.to_sym unless @fields.include? key.to_sym
 
         attr_accessor key.to_sym
       end
